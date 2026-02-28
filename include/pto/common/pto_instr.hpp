47486a04a6b5e21332b93e2c0229faf8ee928d58
Ruoyu Zhou <ruoyu.zhou@hisilicon.com>
2026-02-28 19:47:44 +0800

    cpu-sim: include TMrgSort types for pto_instr.hpp

--- include/pto/common/pto_instr.hpp
+++ include/pto/common/pto_instr.hpp
@@ -16,6 +16,10 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #include "pto/common/pto_instr_impl.hpp"
 #include "pto/comm/comm_types.hpp"
 
+#ifdef __CPU_SIM
+#include "pto/cpu/TMrgSort.hpp"
+#endif
+
 #define MAP_INSTR_IMPL(API, ...) API##_IMPL(__VA_ARGS__)
 
 namespace pto {
