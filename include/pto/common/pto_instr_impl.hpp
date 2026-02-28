3307b7501ee2d658abc6c790791c8e441a0cebf2
Ruoyu Zhou <ruoyu.zhou@hisilicon.com>
2026-02-28 19:48:55 +0800

    fix: close PTO_NPU_ARCH_A2A3 block in pto_instr_impl.hpp

--- include/pto/common/pto_instr_impl.hpp
+++ include/pto/common/pto_instr_impl.hpp
@@ -109,6 +109,8 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #include "pto/npu/a2a3/TColExpandExpdif.hpp"
 #include "pto/npu/a2a3/TQuant.hpp"
 #endif
+#endif // PTO_NPU_ARCH_A2A3
+
 
 #ifdef PTO_NPU_ARCH_A5
 #include "pto/npu/a5/TAssign.hpp"
